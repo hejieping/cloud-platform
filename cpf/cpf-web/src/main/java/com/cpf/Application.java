a47e84d021507a11c3883f1fb18df1fa833ce863
何杰平 <hejieping4399@qq.com>
2018-04-05 18:08:41 +0800

    模块重命名

--- cpf/cpf-web/src/main/java/com/cpf/Application.java
+++ cpf/cpf-web/src/main/java/com/cpf/Application.java
@@ -1,16 +1,11 @@
 package com.cpf;
 
-import com.cpf.logger.BusinessLogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class Application {
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(Application.class);
-		BusinessLogger.infoLog("test",null,"success",null,logger);
 		SpringApplication.run(Application.class, args);
 	}
 }
